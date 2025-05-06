ef11e8c1fffff36bdf40a4c0b9c6e3efe5746986
SirYwell <hannesgreule@outlook.de>
2025-05-06 22:37:50 +0200

    Fix semantic analysis of negative hexadecimals

--- src/main/java/edu/kit/kastel/vads/compiler/parser/ast/LiteralTree.java
+++ src/main/java/edu/kit/kastel/vads/compiler/parser/ast/LiteralTree.java
@@ -14,14 +14,18 @@ public record LiteralTree(String value, int base, Span span) implements Expressi
     }
 
     public OptionalLong parseValue() {
-        int begin = 0;
         int end = value.length();
-        if (base == 16) {
-            begin = 2; // ignore 0x
-        }
+        return switch (base) {
+            case 16 -> parseHex(end);
+            case 10 -> parseDec(end);
+            default -> throw new IllegalArgumentException("unexpected base " + base);
+        };
+    }
+
+    private OptionalLong parseDec(int end) {
         long l;
         try {
-            l = Long.parseLong(value, begin, end, base);
+            l = Long.parseLong(value, 0, end, base);
         } catch (NumberFormatException _) {
             return OptionalLong.empty();
         }
@@ -29,6 +33,14 @@ public record LiteralTree(String value, int base, Span span) implements Expressi
             return OptionalLong.empty();
         }
         return OptionalLong.of(l);
+    }
+
+    private OptionalLong parseHex(int end) {
+        try {
+            return OptionalLong.of(Integer.parseUnsignedInt(value, 2, end, 16));
+        } catch (NumberFormatException e) {
+            return OptionalLong.empty();
+        }
     }
 
 }
