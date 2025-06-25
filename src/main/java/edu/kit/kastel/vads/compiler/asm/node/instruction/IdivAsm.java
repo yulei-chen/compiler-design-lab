22a8a06478b536f8a6d79e538f6818795ce98661
Yulei Chen <chenyuleiw@gmail.com>
2025-06-26 00:33:39 +0200

    fix(asm): invalid suffix for idiv

--- src/main/java/edu/kit/kastel/vads/compiler/asm/node/instruction/IdivAsm.java
+++ src/main/java/edu/kit/kastel/vads/compiler/asm/node/instruction/IdivAsm.java
@@ -25,6 +25,6 @@ public class IdivAsm implements InstructionAsm {
 
     @Override
     public String toString() {
-        return "idivl " + operand.toString();
+        return "idiv " + operand.toString();
     }
 }
